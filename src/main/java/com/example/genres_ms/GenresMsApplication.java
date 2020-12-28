95c043987dcfb3e216ef052ba9ad5d86752b09c4
Tomas Marlein <tomas.marlein@gmail.com>
2020-12-28 22:54:51 +0100

    Advanced Programming Topics

---
+++ src/main/java/com/example/genres_ms/GenresMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.genres_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenresMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenresMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/genres_ms/GenresMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genres_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenresMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
